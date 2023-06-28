529a8157305873e68693035b7381a9841b12a5b1
Joe D <joedark03@gmail.com>
2023-06-27 21:52:38 -0400

    open links in a new tab

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
       <Card.Body>
         <Card.Title style={{fontSize: '1.75rem', color: 'blueviolet'}}><strong><u>Joseph Dark</u></strong></Card.Title>
         <Card.Text style={{ color: 'blueviolet'}}>Find out what is happening beneath the torrent...</Card.Text>
-        <Button className="buttonSpace" variant="primary" href="https://github.com/JoeDark">GitHub</Button>
-        <Button className="buttonSpace" variant="primary" href="https://www.linkedin.com/in/joseph-dark/">Linkedin</Button>
+        <Button className="buttonSpace" variant="primary" target="_blank" href="https://github.com/JoeDark">GitHub</Button>
+        <Button className="buttonSpace" variant="primary" target="_blank" href="https://www.linkedin.com/in/joseph-dark/">Linkedin</Button>
       </Card.Body>
     </Card>       
       </div>
