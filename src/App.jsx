a6b77f6aec3f319fce084328698891fd243851b6
Joe D <joedark03@gmail.com>
2023-12-11 21:08:46 -0500

    start to make UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,38 @@ import './App.css'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
-      <div className='mainText bordered'>
-
-        <Card style={{ background: '#00000000', width: '18rem', borderRadius: '25px', borderColor: 'rgba(0, 0, 0, 0)' }}>
-      <Card.Body>
-        <Card.Title style={{fontSize: '1.75rem', color: 'blueviolet'}}><strong><u>Joseph Dark</u></strong></Card.Title>
-        <Card.Text style={{ color: 'blueviolet'}}>Find out what is happening beneath the torrent...</Card.Text>
-        <Button className="buttonSpace" variant="primary" target="_blank" href="https://github.com/JoeDark">GitHub</Button>
-        <Button className="buttonSpace" variant="primary" target="_blank" href="https://www.linkedin.com/in/joseph-dark/">Linkedin</Button>
-      </Card.Body>
-    </Card>       
+      <Container fluid style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none'}}>
+        <div style={{ position: 'absolute', top: '20%', pointerEvents: 'auto' }}>
+          <h1 >Joseph Dark</h1>
+        </div>
+        <div style={{ position: 'absolute', top: '50%', pointerEvents: 'auto'}}>
+          <h2>Full-Stack software developer, Tech curious</h2>
+        <h3>Find out what is happening beneath the torrent...</h3>
       </div>
+      </Container>
+
+
     </>
   )
 }
 
 export default App
+
+
+{/* <div className='mainText bordered'>
+      
+<Card style={{ background: '#00000000', width: '18rem', borderRadius: '25px', borderColor: 'rgba(0, 0, 0, 0)' }}>
+<Card.Body>
+<Card.Title style={{fontSize: '1.75rem', color: 'blueviolet'}}><strong><u>Joseph Dark</u></strong></Card.Title>
+<Card.Text style={{ color: 'blueviolet'}}>Find out what is happening beneath the torrent...</Card.Text>
+<Button className="buttonSpace" variant="primary" target="_blank" href="https://github.com/JoeDark">GitHub</Button>
+<Button className="buttonSpace" variant="primary" target="_blank" href="https://www.linkedin.com/in/joseph-dark/">Linkedin</Button>
+</Card.Body>
+</Card>       
+</div> */}
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Canvas } from '@react-three/fiber'
-import TestWrapper from './testWrapper.jsx'
 import SeaComponent from './components/SeaComponent.jsx'
 import { OrbitControls } from '@react-three/drei'
 // import {EffectComposer} from '@react-three/postprocessing'
@@ -11,7 +10,9 @@ import { OrbitControls } from '@react-three/drei'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Canvas className='webgl'
-      camera={{ position: [1.2, .23, 0] }}>
+      camera={{ position: [1.2, .23, 0] }}
+      style={{ position: 'absolute', width: '100%', height: '100%', zIndex: '-1' }}>
+        <OrbitControls/>
       <SeaComponent />
     </Canvas>
     <App />
