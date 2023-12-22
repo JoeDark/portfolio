8f9e1043674f6198796abe07250b1e72251bae40
Joe D <joedark03@gmail.com>
2023-12-21 19:16:44 -0500

    some file cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
+import { Canvas } from '@react-three/fiber'
+import SeaComponent from './components/SeaComponent.jsx'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
 
   return (
     <>
-      <Container fluid style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
+      <Container fluid style={{display: 'flex', justifyContent: 'center', pointerEvents: 'none' }}>
         <div className='frosted-glass-container' style={{ position: 'absolute', top: '20%', pointerEvents: 'auto' }}>
           <h1 >Joseph Dark</h1>
         </div>
         <div className='frosted-glass-container' style={{ position: 'absolute', top: '50%', pointerEvents: 'auto' }}>
-          <h2>Full-Stack software developer, Tech curious</h2>
-          <h3>Find out what is happening beneath the torrent...</h3>
-          <div> 
-          <Button className="buttonSpace" variant="primary" target="_blank" href="https://github.com/JoeDark">GitHub</Button>
+          <h2 className='col mb-2'>Full-Stack software developer, Tech curious</h2>
+          <h3 className='col mb-2'>Find out what is happening beneath the torrent...</h3>
+          <div className='col' style={{ display: 'flex', justifyContent: 'space-evenly'}} > 
+          <Button className="buttonSpace" variant="primary" target="_blank" href="https://github.com/JoeDark"> GitHub</Button>
           <Button className="buttonSpace" variant="primary" target="_blank" href="https://www.linkedin.com/in/joseph-dark/">Linkedin</Button>
           </div>
         </div>
       </Container>
 
+      <Canvas className='webgl'
+      camera={{ position: [1.2, .23, 0] }}
+      style={{ position: 'absolute', width: '100%', height: '100%', zIndex: '-1' }}>
+        <OrbitControls/>
+      <SeaComponent />
+    </Canvas>
 
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,10 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Canvas } from '@react-three/fiber'
-import SeaComponent from './components/SeaComponent.jsx'
-import { OrbitControls } from '@react-three/drei'
-// import {EffectComposer} from '@react-three/postprocessing'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Canvas className='webgl'
-      camera={{ position: [1.2, .23, 0] }}
-      style={{ position: 'absolute', width: '100%', height: '100%', zIndex: '-1' }}>
-        <OrbitControls/>
-      <SeaComponent />
-    </Canvas>
     <App />
   </React.StrictMode>,
 )
